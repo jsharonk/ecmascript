36f4bc22713b21abadbe4548eecfba5fb830352d
jeunlee <junniesu@gmail.com>
2016-11-30 11:28:32 -0500

    completed our cool battle game

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
@@ -11,35 +11,54 @@ var Character = function () {
     this.name = name;
     this.attackPower = attackPower;
     this.healthPower = healthPower;
+    this.winnerArr = [];
   }
 
   _createClass(Character, [{
-    key: "attack",
+    key: 'attack',
     value: function attack(opposingPlayer) {
-      if (Math.random > 0.5) {
+      if (Math.random() > 0.5) {
         return opposingPlayer.healthPower = opposingPlayer.healthPower - this.attackPower;
       } else {
         return this.healthPower = this.healthPower - opposingPlayer.attackPower;
       }
     }
   }, {
-    key: "battle",
+    key: 'battle',
     value: function battle(opposingPlayer) {
-
       while (opposingPlayer.healthPower >= 0 && this.healthPower >= 0) {
         this.attack(opposingPlayer);
       }
-      return opposingPlayer.healthPower > 0 ? opposingPlayer.name : this.name;
+      var winnerName = opposingPlayer.healthPower > 0 ? opposingPlayer.name : this.name;
+      this.winnerArr.push(winnerName);
+      return 'The winner is ' + winnerName + '!!!';
+    }
+  }, {
+    key: 'broadcastScore',
+    value: function broadcastScore() {
+      this.winnerArr.map(function (name) {
+        name;
+      });
+      // console.log(this.winnerArr);
+      return this.winnerArr;
     }
   }]);
 
   return Character;
 }();
 
-var red = new Character(attackPower, healthPower, name);
-var blue = new Character(attackPower, healthPower, name);
+// let winnerArr = [];
+// winnerArr.map(name => {name});
 
-var winnerArr = [];
-winnerArr.map(item, function (index) {
-  winner: item;
-});
+(function () {
+  var red = new Character(10, 100, 'Sharon');
+  var blue = new Character(10, 100, 'Jenny');
+
+  for (var i = 10; i > 0; i--) {
+    red.battle(blue);
+    red.healthPower = 100;
+    blue.healthPower = 100;
+  }
+
+  console.log(red.broadcastScore());
+})();
--- index.js
+++ index.js
@@ -2,29 +2,48 @@ class Character {
   constructor(attackPower, healthPower, name) {
     this.name = name;
     this.attackPower = attackPower;
-    this.healthPower = healthPower
+    this.healthPower = healthPower;
+    this.winnerArr = [];
   }
 
   attack(opposingPlayer) {
-    if (Math.random > 0.5) {
+    if (Math.random() > 0.5) {
       return opposingPlayer.healthPower = opposingPlayer.healthPower - this.attackPower;
     } else {
       return this.healthPower = this.healthPower - opposingPlayer.attackPower;
     }
   }
-  battle(opposingPlayer) {
 
+  battle(opposingPlayer) {
     while (opposingPlayer.healthPower >= 0 && this.healthPower >= 0) {
       this.attack(opposingPlayer);
     }
-    return (opposingPlayer.healthPower > 0) ? opposingPlayer.name : this.name;
-
+    let winnerName = (opposingPlayer.healthPower > 0) ? opposingPlayer.name : this.name;
+    this.winnerArr.push(winnerName);
+    return `The winner is ${winnerName}!!!`;
  }
+
+  broadcastScore(){
+    this.winnerArr.map(name => {name});
+    // console.log(this.winnerArr);
+    return this.winnerArr;
+  }
+
 }
 
-let red = new Character(attackPower, healthPower, name);
-let blue = new Character(attackPower, healthPower, name);
+// let winnerArr = [];
+// winnerArr.map(name => {name});
+
+(function(){
+  let red = new Character(10, 100, 'Sharon');
+  let blue = new Character(10, 100, 'Jenny');
 
 
-let winnerArr = [];
-winnerArr.map(item, index => {winner: item});
+  for (var i = 10; i > 0; i--) {
+    red.battle(blue);
+    red.healthPower = 100;
+    blue.healthPower = 100;
+  }
+
+  console.log(red.broadcastScore());
+})();
